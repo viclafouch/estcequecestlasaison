89c75b1b728bb16b262b87adc53e593db60b200c
viclafouch <victor.dlf@outlook.fr>
2026-01-31 21:36:00 +0100

    fix: devtools only in dev

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,6 +2,7 @@ import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 
@@ -21,6 +22,7 @@ export default defineConfig({
       projects: ['./tsconfig.json']
     }),
     tailwindcss(),
+    devtools(),
     tanstackStart(),
     nitro({
       routeRules: {
